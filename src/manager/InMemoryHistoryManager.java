5d2dc48adfe7d2aaf9c4bbe86ff28706d0c8ed7a
Mikhail Ignatenko <ignatenkomikhail239@gmail.com>
2024-12-24 18:06:29 +0300

    Первое ревью

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -3,25 +3,91 @@ package manager;
 import model.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.NoSuchElementException;
 
 public class InMemoryHistoryManager implements HistoryManager {
     private ArrayList<Task> tasks;
+    private HashMap<Integer, Node<Task>> taskTracker;
+    private Node<Task> head;
+    private Node<Task> tail;
+
+    class Node<Task>  {
+        public Task data;
+        public Node<Task> prev;
+        public Node<Task> next;
+
+        public Node(Node<Task> prev, Task data, Node<Task> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
 
     public InMemoryHistoryManager() {
         tasks = new ArrayList<>();
+        taskTracker = new HashMap<>();
+    }
+
+    private Node<Task> linkLast(Task element) {
+       final Node<Task> oldTail = tail;
+       final Node<Task> newNode = new Node<>(oldTail, element, null);
+       tail = newNode;
+       if (oldTail == null) {
+           head = newNode;
+       } else {
+           oldTail.next = newNode;
+       }
+       return newNode;
+    }
+
+    private void removeNode(Node<Task> node) {
+        Node<Task> previousNode = node.prev;
+        Node<Task> nextNode = node.next;
+        if (nextNode == null & previousNode != null) {
+            tail = previousNode;
+            previousNode.next = null;
+        } else if (previousNode == null & nextNode != null) {
+            head = nextNode;
+            nextNode.prev = null;
+        } else if (previousNode == null & nextNode == null) {
+            tail = null;
+            head = null;
+        } else {
+            previousNode.next = nextNode;
+            nextNode.prev = previousNode;
+        }
+        node.prev = null;
+        node.next = null;
+        node.data = null;
+    }
+
+    private void getTasks(){
+        tasks.clear();
+        for (Node<Task> node: taskTracker.values()) {
+            tasks.add(node.data);
+        }
     }
 
     @Override
     public ArrayList<Task> getHistory() {
+        getTasks();
         return new ArrayList<>(tasks);
     }
 
     @Override
     public void add(Task task) {
-        if (tasks.size()>=10) {
-            tasks.remove(0);
+        if (taskTracker.containsKey(task.getID())) {
+            removeNode(taskTracker.get(task.getID()));
+            remove(task.getID());
         }
-        tasks.add(task);
+        taskTracker.put(task.getID(), linkLast(task));
+
+    }
+
+    @Override
+    public void remove(int id) {
+        taskTracker.remove(id);
     }
 
 }
--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -23,4 +23,73 @@ class InMemoryHistoryManagerTest {
         assertEquals(1, history.size(),"Кол-во элементов не совпадает с ожидаемым");
     }
 
+    @Test
+    public void shouldAddOnlyOneInstanceOfTask() {
+        Task task = new Task(1, "Title", "Description", TaskStatus.DONE);
+        historyManager.add(task);
+        historyManager.add(task);
+        final ArrayList<Task> history = historyManager.getHistory();
+        assertNotNull(history, "История не создана");
+        assertEquals(1, history.size(),"Кол-во элементов не совпадает с ожидаемым");
+    }
+
+    @Test
+    public void shouldReplaceTaskInTheEnd() {
+        Task task1 = new Task(1, "Title", "Description", TaskStatus.DONE);
+        Task task2 = new Task(2, "Title", "Description", TaskStatus.DONE);
+        Task task3 = new Task(3, "Title", "Description", TaskStatus.DONE);
+        Task task4 = new Task(3, "Title", "Description", TaskStatus.DONE);
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+        historyManager.add(task4);
+        final ArrayList<Task> history = historyManager.getHistory();
+        assertNotNull(history, "История не создана");
+        assertEquals(3, history.size(),"Кол-во элементов не совпадает с ожидаемым");
+        assertEquals(history.get(2), task4, "Задание не совпадает с ожидаемым");
+    }
+
+    @Test
+    public void shouldReplaceTaskInTheMiddle() {
+        Task task1 = new Task(1, "Title", "Description", TaskStatus.DONE);
+        Task task2 = new Task(2, "Title", "Description", TaskStatus.DONE);
+        Task task3 = new Task(3, "Title", "Description", TaskStatus.DONE);
+        Task task4 = new Task(2, "Title", "Description", TaskStatus.DONE);
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+        historyManager.add(task4);
+        final ArrayList<Task> history = historyManager.getHistory();
+        assertNotNull(history, "История не создана");
+        assertEquals(3, history.size(),"Кол-во элементов не совпадает с ожидаемым");
+        assertEquals(history.get(1), task4, "Задание не совпадает с ожидаемым");
+    }
+
+    @Test
+    public void shouldReplaceTaskInTheStart() {
+        Task task1 = new Task(1, "Title", "Description", TaskStatus.DONE);
+        Task task2 = new Task(2, "Title", "Description", TaskStatus.DONE);
+        Task task3 = new Task(3, "Title", "Description", TaskStatus.DONE);
+        Task task4 = new Task(1, "Title", "Description", TaskStatus.DONE);
+        historyManager.add(task1);
+        historyManager.add(task2);
+        historyManager.add(task3);
+        historyManager.add(task4);
+        final ArrayList<Task> history = historyManager.getHistory();
+        assertNotNull(history, "История не создана");
+        assertEquals(3, history.size(),"Кол-во элементов не совпадает с ожидаемым");
+        assertEquals(history.get(0), task4, "Задание не совпадает с ожидаемым");
+    }
+
+    @Test
+    public void shouldDeleteTaskById() {
+        Task task1 = new Task(1, "Title", "Description", TaskStatus.DONE);
+        historyManager.add(task1);
+        historyManager.remove(1);
+        final ArrayList<Task> history = historyManager.getHistory();
+        assertTrue(history.isEmpty(), "История не пуста");
+    }
+
+
+
 }
